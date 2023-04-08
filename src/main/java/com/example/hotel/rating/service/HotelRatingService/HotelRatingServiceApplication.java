887fb1a75a23ee5dff4eab59dbc1421511eea689
ashish <ashishchoksi84@gmail.com>
2023-04-08 19:07:36 +0530

    initial setup

---
+++ src/main/java/com/example/hotel/rating/service/HotelRatingService/HotelRatingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.hotel.rating.service.HotelRatingService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelRatingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelRatingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hotel/rating/service/HotelRatingService/HotelRatingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hotel.rating.service.HotelRatingService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelRatingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
