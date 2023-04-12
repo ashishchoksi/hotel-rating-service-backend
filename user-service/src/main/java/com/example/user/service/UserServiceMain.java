9559044f7418cef567ce37711d1b2f0cb453f3a2
ashish <ashishchoksi84@gmail.com>
2023-04-12 22:59:21 +0530

    enable eureka client for user-service

--- user-service/src/main/java/com/example/user/service/UserServiceMain.java
+++ user-service/src/main/java/com/example/user/service/UserServiceMain.java
@@ -2,8 +2,10 @@ package com.example.user.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class UserServiceMain {
     public static void main(String[] args) {
         SpringApplication.run(UserServiceMain.class, args);
