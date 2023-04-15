d8a0592195b5912ad93820319d22db2a1d3255de
ashish <ashishchoksi84@gmail.com>
2023-04-15 14:10:20 +0530

    set rest teamplate for services comminication

--- user-service/src/main/java/com/example/user/service/services/impl/UserServiceImpl.java
+++ user-service/src/main/java/com/example/user/service/services/impl/UserServiceImpl.java
@@ -1,18 +1,27 @@
 package com.example.user.service.services.impl;
 
+import com.example.user.service.entities.Hotel;
+import com.example.user.service.entities.Rating;
 import com.example.user.service.entities.User;
 import com.example.user.service.exceptions.ResourceNotFoundException;
 import com.example.user.service.repositories.UserRepository;
 import com.example.user.service.services.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
+    private final RestTemplate restTemplate;
 
     @Override
     public User save(User user) {
@@ -28,6 +37,21 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User getUser(String userId) {
-        return userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("No user found with id: " + userId));
+        User user = userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("No user found with id: " + userId));
+        List<Rating> userRating = getUserRating(userId);
+        userRating.forEach(rating -> {
+            String hotelServiceUrl = String.format("http://localhost:8082/hotels/%s", rating.getHotelId());
+            Hotel hotel = restTemplate.getForObject(hotelServiceUrl, Hotel.class);
+            rating.setHotel(hotel);
+        });
+        user.setRatings(userRating);
+        return user;
+    }
+
+    private List<Rating> getUserRating(String userId) {
+        String ratingServiceUrl = String.format("http://localhost:8083/ratings/users/%s", userId);
+        log.info("making API call to service: {}", ratingServiceUrl);
+        Rating[] ratings = restTemplate.getForObject(ratingServiceUrl, Rating[].class);
+        return Arrays.stream(ratings).collect(Collectors.toList());
     }
 }
