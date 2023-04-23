697dd1aab5ccaeb8ace588532d864ac1289e2c86
ashish <ashishchoksi84@gmail.com>
2023-04-23 13:02:39 +0530

    implement rate limiter in hotel-service using resilience4j

--- hotel-service/src/main/java/com/example/hotel/service/controllers/HotelController.java
+++ hotel-service/src/main/java/com/example/hotel/service/controllers/HotelController.java
@@ -2,7 +2,10 @@ package com.example.hotel.service.controllers;
 
 import com.example.hotel.service.entities.Hotel;
 import com.example.hotel.service.services.HotelService;
+import com.google.common.collect.ImmutableList;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/hotels")
 @RequiredArgsConstructor
+@Slf4j
 public class HotelController {
 
     private final HotelService hotelService;
@@ -28,9 +32,15 @@ public class HotelController {
     }
 
     @GetMapping("")
+    @RateLimiter(name = "hotelsRateLimit", fallbackMethod = "hotelLimitExceed")
     public ResponseEntity<List<Hotel>> getAllHotels() {
         List<Hotel> hotels = hotelService.getAllHotels();
         return ResponseEntity.ok(hotels);
+    }
+
+    public ResponseEntity<List<Hotel>> hotelLimitExceed(Exception ex) {
+        log.error("Too many request from user...");
+        return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).body(ImmutableList.of());
     }
 
 }
