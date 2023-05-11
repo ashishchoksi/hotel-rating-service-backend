01ec7900751d7c6a8a6e7dcacaa17d655529e00b
Ashish Choksi <ashish@192.168.1.10>
2023-05-11 14:30:08 +0530

    Enable swagger ui in user and rating service

--- rating-service/src/main/java/com/example/rating/service/controllers/RatingController.java
+++ rating-service/src/main/java/com/example/rating/service/controllers/RatingController.java
@@ -2,6 +2,8 @@ package com.example.rating.service.controllers;
 
 import com.example.rating.service.entities.Rating;
 import com.example.rating.service.services.RatingService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,31 +14,37 @@ import java.util.List;
 @RestController
 @RequestMapping("/ratings")
 @RequiredArgsConstructor
+@Tag(name = "Rating service")
 public class RatingController {
 
     private final RatingService ratingService;
 
     @PostMapping("")
+    @Operation(summary = "create ratings", description = "create the ratings")
     public ResponseEntity<Rating> create(@RequestBody Rating rating) {
         return ResponseEntity.status(HttpStatus.CREATED).body(ratingService.save(rating));
     }
 
     @GetMapping("/{ratingId}")
+    @Operation(summary = "get rating by id", description = "get single rating by id")
     public ResponseEntity<Rating> getRating(@PathVariable String ratingId) {
         return ResponseEntity.ok(ratingService.getRating(ratingId));
     }
 
     @GetMapping("")
+    @Operation(summary = "get all ratings", description = "get all ratings")
     public ResponseEntity<List<Rating>> getAllRatings() {
         return ResponseEntity.ok(ratingService.getAllRatings());
     }
 
     @GetMapping("users/{userId}")
+    @Operation(summary = "get all ratings for given user", description = "get all ratings for user")
     public ResponseEntity<List<Rating>> getRatingsByUserId(@PathVariable String userId) {
         return ResponseEntity.ok(ratingService.getRatingByUserId(userId));
     }
 
     @GetMapping("hotels/{hotelId}")
+    @Operation(summary = "get all ratings for given hotel", description = "get all the ratings for given hotel")
     public ResponseEntity<List<Rating>> getRatingsByHotelId(@PathVariable String hotelId) {
         return ResponseEntity.ok(ratingService.getRatingByHotelId(hotelId));
     }
--- user-service/src/main/java/com/example/user/service/controllers/UserController.java
+++ user-service/src/main/java/com/example/user/service/controllers/UserController.java
@@ -5,6 +5,8 @@ import com.example.user.service.services.UserService;
 import com.google.common.collect.ImmutableList;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -17,16 +19,19 @@ import java.util.List;
 @RequestMapping("/users")
 @RequiredArgsConstructor
 @Slf4j
+@Tag(name = "user service")
 public class UserController {
     private final UserService userService;
 
     @PostMapping("")
+    @Operation(summary = "create the user", description = "create the user")
     public ResponseEntity<User> createUser(@RequestBody User user) {
         User newUser = userService.save(user);
         return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
     }
 
     @GetMapping("/{userId}")
+    @Operation(summary = "get user by id", description = "get user by id")
     @CircuitBreaker(name = "ratingHotelBreaker", fallbackMethod = "ratingFailureFallback")
     public ResponseEntity<User> getUser(@PathVariable String userId) {
         User user = userService.getUser(userId);
@@ -39,6 +44,7 @@ public class UserController {
     }
 
     @GetMapping("")
+    @Operation(summary = "get all the users", description = "get all the users")
     @Retry(name = "userRatingService", fallbackMethod = "retryLimitExceed")
     public ResponseEntity<List<User>> getAllUsers() {
         log.info("Getting all the users...");
